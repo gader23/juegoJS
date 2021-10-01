a6e70abd00aebc709a010c6849b378dc3c6b9625
Gabriel Cabrera <gabycabper237@gmail.com>
2021-10-01 20:15:32 +0200

    Sesion 16

--- js/juego.js
+++ js/juego.js
@@ -1,24 +1,67 @@
-var personaje=function(x,y,nombre)
+
+var canvas;
+var ctx;
+var FPS=50;
+
+
+var personaje=function(x,y)
 {
   this.x=x;
   this.y=y;
-  this.nombre=nombre;
+  this.derecha=true;
 
-//METODO "ABAJO"
-  this.abajo=function()
-  {
-    this.y+=10;
+  this.dibuja=function(){
+    ctx.fillstyle = '#FFF000';
+    ctx.fillRect(this.x,this.y,50,50);
   }
 
-  //METODO  "HABLAR"
-  this.hablar=function()
-  {
-    console.log('Hola forastero, me llamo ' + this.nombre);
+  this.mueve=function(velocidad){
+    if(this.derecha==true){
+      if(this.x<400){
+      this.x+=velocidad;
+      }else {
+      this.derecha=false;
+      }
+    }else {
+      if (this.x>50) {
+        this.x-=velocidad;
+      }else {
+        this.derecha=true;
+      }
+    }
   }
 }
 
-var personaje1=new personaje(10,100,'Frodo');
-var personaje2=new personaje(220,380,'Sam');
+var per1=new personaje(10,50);
+var per2=new personaje(10,120);
+var per3=new personaje(10,230);
 
-personaje1.x=556;
-personaje1.abajo();
+function inicializa()
+{
+  canvas=document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  setInterval(function()
+  {
+    principal();
+  },1000/FPS);
+}
+
+function borrarCanvas(){
+  canvas.width=500;
+  canvas.height=400;
+}
+
+function principal()
+{
+  borrarCanvas();
+
+
+  per1.dibuja();
+  per2.dibuja();
+  per3.dibuja();
+
+  per1.mueve(1);
+  per2.mueve(3);
+  per3.mueve(7);
+  //console.log('funcion');
+}
