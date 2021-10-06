2e82964d5ed6be9118aaf8797610c48e6837b5e1
Gabriel Cabrera <gabycabper237@gmail.com>
2021-10-06 19:30:12 +0200

    Sesion 22

--- js/juego.js
+++ js/juego.js
@@ -10,6 +10,13 @@ var puerta='#3a1700';
 var tierra='#c6892f';
 var llave='#c6bc00';
 
+var protagonista;
+
+var enemigo=[];
+
+var tileMap;
+
+var imagenAntorcha;
 
 var escenario=[
   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -25,27 +32,117 @@ var escenario=[
 ];
 
 function dibujaEscenario(){
-  var color;
+
   for(y=0;y<10;y++){
     for(x=0;x<15;x++){
-      if(escenario[y][x]==0){
-        color=muro;
-      }
-      if(escenario[y][x]==1){
-        color=puerta;
-      }
-      if(escenario[y][x]==2){
-        color=tierra;
-      }
-      if (escenario[y][x]==3) {
-        color=llave
-      }
-      ctx.fillStyle=color;
-      ctx.fillRect(x*anchoF,y*altoF,anchoF,altoF);
+      var tile=escenario[y][x];
+      //ctx.fillStyle=color;
+      //ctx.fillRect(x*anchoF,y*altoF,anchoF,altoF);
+      ctx.drawImage(tileMap,tile*32,0,32,32,anchoF*x,altoF*y,anchoF,altoF);
     }
   }
 }
 
+var antorcha=function(x,y){
+  this.x=x;
+  this.y=y;
+
+  this.fotograma=0;//0-3
+  this.contador=0;
+  this.retraso=10;
+
+  this.cambiaFotograma=function(){
+    if(this.fotograma<3){
+      this.fotograma++;
+    }else {
+      this.fotograma=0;
+    }
+  }
+
+  this.dibuja=function(){
+
+    if(this.contador<this.retraso){
+      this.contador++;
+    }else{
+      this.contador=0;
+      this.cambiaFotograma();
+    }
+    ctx.drawImage(tileMap,this.fotograma*32,64,32,32,anchoF*x,altoF*y,anchoF,altoF);
+
+  }
+}
+
+//CLASE ENEMIGO
+
+var malo=function(x,y){
+  this.x=x;
+  this.y=y;
+
+  this.direccion =Math.floor(Math.random()*4);
+
+  this.retraso=50;
+  this.contador=0;
+
+  this.dibuja=function(){
+      ctx.drawImage(tileMap,0,32,32,32,this.x*anchoF,this.y*altoF,anchoF,altoF);
+  }
+
+  this.compruebaColision=function(x,y){
+    var colisiona=false;
+
+    if(escenario[y][x]==0){
+      colisiona=true;
+    }
+    return colisiona;
+  }
+
+  this.mueve=function(){
+
+    protagonista.colisionEnemigo(this.x,this.y);
+
+    if(this.contador < this.retraso){
+      this.contador++;
+    }else {
+      this.contador=0;
+      //ARRIBA
+      if (this.direccion==0) {
+        if (this.compruebaColision(this.x,this.y-1)==false) {
+          this.y--;
+        }else {
+          this.direccion =Math.floor(Math.random()*4);
+        }
+      }
+
+      //ABAJO
+      if (this.direccion==1) {
+        if (this.compruebaColision(this.x,this.y+1)==false) {
+          this.y++;
+        }else {
+          this.direccion =Math.floor(Math.random()*4);
+        }
+      }
+
+
+      //IZQUIERDA
+      if (this.direccion==2) {
+        if (this.compruebaColision(this.x-1,this.y)==false) {
+          this.x--;
+        }else {
+          this.direccion =Math.floor(Math.random()*4);
+        }
+      }
+
+      //DERECHA
+      if (this.direccion==3) {
+        if (this.compruebaColision(this.x+1,this.y)==false) {
+          this.x++;
+        }else {
+          this.direccion =Math.floor(Math.random()*4);
+        }
+      }
+    }
+  }
+}
 
 //OBJETO JUGADOR
 var jugador=function(){
@@ -56,8 +153,15 @@ var jugador=function(){
 
 
   this.dibuja=function(){
-    ctx.fillStyle=this.color;
-    ctx.fillRect(this.x*anchoF,this.y*altoF,anchoF,altoF);
+      ctx.drawImage(tileMap,32,32,32,32,this.x*anchoF,this.y*altoF,anchoF,altoF);
+    //ctx.fillStyle=this.color;
+    //ctx.fillRect(this.x*anchoF,this.y*altoF,anchoF,altoF);
+  }
+
+  this.colisionEnemigo=function(x,y){
+    if(this.x==x&&this.y==y){
+      this.muerte();
+    }
   }
 
   this.margenes=function(x,y){
@@ -104,6 +208,14 @@ var jugador=function(){
     escenario[8][3]=3;
   }
 
+  this.muerte=function(){
+    console.log('Has muerto!');
+    this.x=1;
+    this.y=1;
+    this.llave=false;
+    escenario[8][3]=3;
+  }
+
   this.logicaObjetos=function(){
     var objeto =  escenario[this.y][this.x];
     //OBTIENE LLAVE
@@ -123,16 +235,23 @@ var jugador=function(){
   }
 }
 
-var protagonista;
-
-
 
 function inicializa(){
   canvas=document.getElementById('canvas');
   ctx=canvas.getContext('2d');
 
+  tileMap=new Image();
+  tileMap.src='img/tilemap.png';
+
   //CREAMOS AL JUGADOR
   protagonista = new jugador();
+
+  //CREAMOS ANTORCHA
+  imagenAntorcha=new antorcha(0,0);
+  //CREAMOS LOS ENEMIGOS
+  enemigo.push(new malo(3,3));
+  enemigo.push(new malo(5,7));
+  enemigo.push(new malo(7,7));
 
   //LECTURA DEL TECLADO
   document.addEventListener('keydown',function(tecla){
@@ -168,4 +287,9 @@ function principal() {
   borrarCanvas();
   dibujaEscenario();
   protagonista.dibuja();
+  imagenAntorcha.dibuja();
+  for (c = 0; c < enemigo.length; c++) {
+    enemigo[c].mueve();
+    enemigo[c].dibuja();
+  }
 }
