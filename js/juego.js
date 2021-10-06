a766802503a4725c29c6d37d6404620c46049d26
Gabriel Cabrera <gabycabper237@gmail.com>
2021-10-06 18:30:19 +0200

    Sesion 21

--- js/juego.js
+++ js/juego.js
@@ -12,22 +12,22 @@ var llave='#c6bc00';
 
 
 var escenario=[
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,2,2,0,0,0,2,2,0,0],
-  [0,0,2,2,2,2,2,0,0,0],
-  [0,0,2,0,0,0,2,2,0,0],
-  [0,0,2,2,2,0,0,2,0,0],
-  [0,2,2,0,0,0,0,2,0,0],
-  [0,0,2,0,0,0,2,2,2,0],
-  [0,2,2,2,0,0,2,0,0,0],
-  [0,2,2,2,0,0,2,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0]
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,2,2,0,0,0,2,2,2,2,0,0,2,2,0],
+  [0,0,2,2,2,2,2,0,0,2,0,0,2,0,0],
+  [0,0,2,0,0,0,2,2,0,2,2,2,2,0,0],
+  [0,0,2,2,2,0,0,2,0,0,0,2,0,0,0],
+  [0,2,2,0,0,0,0,2,0,0,0,2,0,0,0],
+  [0,0,2,0,0,0,2,2,2,0,0,2,2,2,0],
+  [0,2,2,2,0,0,2,0,0,0,1,0,0,2,0],
+  [0,2,2,3,0,0,2,0,0,2,2,2,2,2,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
 
 function dibujaEscenario(){
   var color;
   for(y=0;y<10;y++){
-    for(x=0;x<10;x++){
+    for(x=0;x<15;x++){
       if(escenario[y][x]==0){
         color=muro;
       }
@@ -51,8 +51,9 @@ function dibujaEscenario(){
 var jugador=function(){
   this.x=1;
   this.y=1;
-
   this.color='#820c01';
+  this.llave=false;
+
 
   this.dibuja=function(){
     ctx.fillStyle=this.color;
@@ -68,23 +69,57 @@ var jugador=function(){
   }
 
   this.arriba=function(){
-    if(this.margenes(this.x,this.y-1)==false)
-    this.y--;
+    if(this.margenes(this.x,this.y-1)==false){
+      this.y--;
+      this.logicaObjetos();
+    }
   }
 
   this.abajo=function(){
-    if(this.margenes(this.x,this.y+1)==false)
-    this.y++;
+    if(this.margenes(this.x,this.y+1)==false){
+      this.y++;
+      this.logicaObjetos();
+    }
   }
 
   this.izquierda=function(){
-    if(this.margenes(this.x-1,this.y)==false)
-    this.x--;
+    if(this.margenes(this.x-1,this.y)==false){
+      this.x--;
+      this.logicaObjetos();
+    }
   }
 
   this.derecha=function(){
-    if(this.margenes(this.x+1,this.y)==false)
-    this.x++;
+    if(this.margenes(this.x+1,this.y)==false){
+      this.x++;
+      this.logicaObjetos();
+    }
+  }
+
+  this.victoria=function(){
+    console.log('Has ganado!');
+    this.x=1;
+    this.y=1;
+    this.llave=false;
+    escenario[8][3]=3;
+  }
+
+  this.logicaObjetos=function(){
+    var objeto =  escenario[this.y][this.x];
+    //OBTIENE LLAVE
+    if(objeto == 3){
+      this.llave=true;
+      escenario[this.y][this.x]=2;
+      console.log('Has obtenido la llave!!!');
+    }
+    //ABRIR PUERTA
+    if(objeto==1){
+      if(this.llave==true)
+        this.victoria();
+        else {
+          console.log('TE FALTA LA LLAVE! NO PUEDES PASAR');
+        }
+    }
   }
 }
 
@@ -125,7 +160,7 @@ function inicializa(){
 
 
 function borrarCanvas() {
-  canvas.width=500;
+  canvas.width=750;
   canvas.height=500;
 }
 
